4947bff00eabcf460e92fc50f42de6717fe56493
Athena <athenachang17@gmail.com>
2024-10-10 10:09:18 -0400

    didn't finish 2 and 6. making sure this was up to date with my previous commit because I forgot to commit and push yesterday

--- index.js
+++ index.js
@@ -10,21 +10,41 @@ const multiplesOfSixAndNine = () => {
 multiplesOfSixAndNine();
 
 // 2 not finished
-const greaterNum = (a, b) => {
+let greaterNum = (a, b) => {
     if (typeof a === "number" && typeof b === "number") {
-        if (Number(a) > Number(b)) {
-            return a;
-        } else if (Number(a) === Number(b)) {
+        if (a === b) {
             return "both integers are equal";
+        } else if (a > b) {
+            return a;
+        };
+    };
+
+    if (typeof a === "float" && typeof b === "float") {
+        if (a === b) {
+            return "both integers are equal";
+        } else if (a > b) {
+            return a;
         } else {
             return null;
         };
     };
+
+    if (typeof a === "string" && typeof b === "string") {
+        if (a === b) {
+            return null;
+        };
+    };
+
+    if (typeof a === "string" && typeof b === "number") {
+        if (a !== b) {
+            return null;
+        };
+    };
 };
-console.log(greaterNum(10, 7)); //returns 10
-console.log(greaterNum(1.14, 1.14)); //returns "both integers are equal"
-console.log(greaterNum("21", 21)); //returns null
-console.log(greaterNum("21", "21")); //returns null
+greaterNum(10, 7); //returns 10
+greaterNum(1.14, 1.14); //returns "both integers are equal"
+greaterNum("21", 21); //returns null
+greaterNum("21", "21"); //returns null
 
 // 3
 const sumOfFourAndSix = () => {
@@ -70,16 +90,124 @@ const sumOfNotDivisibleByTen = () => {
 };
 console.log(sumOfNotDivisibleByTen()); //returns 450000
 
-// 6
-// const greaterNum = (a, b, c, d) => {
+// 6 not finished
+const greaterNumb = (a, b, c, d) => {
+    // const integer = (value) => {
+    //     return typeof value === "number" || typeof value === "float";
+    // };
 
-// }
-// greaterNum(10, 7, 16, 80) //returns 80
-// greaterNum(1.14, 1.14, 5, 7) //returns "two integers are equal"
-// greaterNum(1.14, 1.14, 1.14, 7) //returns "three integers are equal"
-// greaterNum("21", 21, 60, 3) //returns null
+    // if (typeof a === "float" && typeof b === "float" && typeof c === "float" && typeof d === "float") {
+    //     //if (typeof a === integer && typeof b === integer && typeof c === integer && typeof d === integer) {
+    //         if (a === b && a === c && a === d) {
+    //             return "all integers are equal";
+    //         } else if (b === c && b === d) {
+    //             return "three integers are equal";
+    //         } else if (c === d) {
+    //             return "two integers are equal";
+    //         } else if (a > b && a > c && a > d) {
+    //             return a;
+    //         } else if (b > c && b > d) {
+    //             return b;
+    //         } else if (c > d) {
+    //             return c;
+    //         } else {
+    //             return d;
+    //         };
+    //     };
 
-// 7 (Bonus)
+    if (typeof a === "float" && typeof b === "float" && typeof c === "float" && typeof d === "float") {
+        if (a === b && a === c && a === d) {
+            return "four integers are equal";
+        } else if (b === c && b === d) {
+            return "three integers are equal";
+        } else if (c === d) {
+            return "two integers are equal";
+        } 
+    };
+
+    // if (typeof a === "number" && typeof b === "number" && typeof c === "number" && typeof d === "number") {
+    // //if (typeof a === integer && typeof b === integer && typeof c === integer && typeof d === integer) {
+    //     if (a === b && a === c && a === d) {
+    //         return "all integers are equal";
+    //     } else if (b === c && b === d) {
+    //         return "three integers are equal";
+    //     } else if (c === d) {
+    //         return "two integers are equal";
+    //     }; 
+    // };
+
+    if (typeof a === "number" && typeof b === "number" && typeof c === "number" && typeof d === "number") {
+    // if (typeof a === integer && typeof b === integer && typeof c === integer && typeof d === integer) {
+        if (a > b && a > c && a > d) {
+            return a;
+        } else if (b > c && b > d) {
+            return b;
+        } else if (c > d) {
+            return c;
+        } else {
+            return d;
+        };
+    };
 
 
-// 8 (Bonus)
+
+    // if (typeof a === "float" && typeof b === "float" && typeof c === "float" && typeof d === "float") {
+    //     if (a > b && a > c && a > d) {
+    //         return a;
+    //     } else if (b > c && b > d) {
+    //         return b;
+    //     } else if (c > d) {
+    //         return c;
+    //     } else {
+    //         return d;
+    //     };
+    // };
+
+    if (typeof a === "string" && typeof b === "string") {
+        if (a === b || a === c || a === d) {
+            return null;
+        } else if (b === c || b === d) {
+            return null;
+        } else {
+            return null;
+        } 
+    };
+
+    if (typeof a === "string" && typeof b === "number") {
+        if (a !== b || a !== c || a !== d) {
+            return null;
+        } else if (b !== c || b !== d) {
+            return null;
+        } else {
+            return null;
+        } 
+    };
+};
+console.log(greaterNumb(10, 7, 16, 80)); //returns 80
+console.log(greaterNumb(1.14, 1.14, 5, 7)); //returns "two integers are equal"
+console.log(greaterNumb(1.14, 1.14, 1.14, 7)); //returns "three integers are equal"
+console.log(greaterNumb("21", 21, 60, 3)); //returns null
+
+// 7 (Bonus) 
+const multiplesOfFourAndSix = () => {
+    let arr = [];
+    for (let i = 1; i <= 100; i++) {
+        if (i % 4 === 0 && i % 6 === 0) {
+            arr.push(i);
+        };
+    };
+    return arr;
+};
+multiplesOfFourAndSix(); // returns [12, 24, 36, 48,60, 72, 84, 96]
+
+// 8 (Bonus) 
+const countMultiplesOfTwo = () => {
+    let arr = [];
+    for (let i = 1; i <= 100; i++) {
+        if (i % 2 === 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+};
+countMultiplesOfTwo();
